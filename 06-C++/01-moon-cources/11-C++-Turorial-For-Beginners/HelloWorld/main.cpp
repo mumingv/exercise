b3355ac76398e22cdccfced15b6e6d6523905d36
Yin Jie <mumingv@163.com>
2023-02-21 15:29:20 +0800

    Finish Video Course: C++ Turorial For Beginners

--- 06-C++/01-moon-cources/11-C++-Turorial-For-Beginners/HelloWorld/main.cpp
+++ 06-C++/01-moon-cources/11-C++-Turorial-For-Beginners/HelloWorld/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand(time(nullptr));
+    int number = rand() % 10;
+    cout << number << ", " << time(0);
+
     return 0;
 }
